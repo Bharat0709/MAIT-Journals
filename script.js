19cd61d35f5adb668208a52a62ed2b72ede8607a
Bharat Pahwa <pahwabharat15@gmail.com>
2023-06-08 14:14:18 +0530

    added filters

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const Detail2 = document.querySelector("#Detail-2");
 const Detail3 = document.querySelector("#Detail-3");
 const Detail4 = document.querySelector("#Detail-4");
 const details = document.querySelectorAll(".image-details");
+const JournalsList = document.querySelector(".Journals-div");
 
 words.forEach((word, index) => {
   const span = document.createElement("span");
@@ -13,42 +14,227 @@ words.forEach((word, index) => {
   span.style.animationDelay = `${index * 0.5}s`;
   tagline.appendChild(span);
 });
-const prevButton = document.querySelector(".prev-button");
-const nextButton = document.querySelector(".next-button");
-const carouselItems = document.querySelectorAll(".carousel-item");
 
-let currentIndex = 2; // Start with the center item
+const journals = [
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Ashish Sharma",
+    publishingDate: "20-03-2022",
+    category: "Computer Science",
+    previewImage: "Images/Paper-1.png",
+    url: "wikipeida.com",
+    type: "top-reads",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Ashish Sharma",
+    publishingDate: "20-03-2023",
+    category: "Computer Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Bharat Pahwa",
+    publishingDate: "20-03-2023",
+    category: "Mechanical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "top-reads",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Electrical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Electrical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Electrical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Electrical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Electrical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Computer Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Electrical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Computer Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "top-reads",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepika",
+    publishingDate: "20-03-2023",
+    category: "Computer Science",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deep",
+    publishingDate: "20-03-2023",
+    category: "Computer Science",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "top-reads",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Kumar",
+    publishingDate: "20-03-2023",
+    category: "Electrical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Electrical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "top-reads",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Electrical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "latest",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Electrical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "top-reads",
+  },
+  {
+    title: "Augmenting Trust by Combining Machine Learning and Blockchain",
+    author: "Deepak Kumar",
+    publishingDate: "20-03-2023",
+    category: "Mechanical Engineering",
+    previewImage: "Images/Paper-1.png",
+    url: "https://docs.google.com/document/d/18syYaxqRP6N8cCX9ZQx4RD0_l1gxnWAzHghp7mIJIic/edit",
+    type: "top-reads",
+  },
+];
 
-function showSlide(index) {
-  carouselItems.forEach((item, i) => {
-    const distance = Math.abs(index - i);
-    item.style.opacity = distance === 0 ? 1 : 0.5;
-  });
+// Function to initialize the filtering functionality
+function initializeFiltering() {
+  // Get the category select element and radio buttons for filtering
+  var categorySelect = document.getElementById("category");
+  var topReadsRadio = document.getElementById("top-reads");
+  var latestRadio = document.getElementById("latest");
 
-  const centerItem = carouselItems[currentIndex];
-  centerItem.classList.remove("center-item");
+  // Function to filter the journals based on the selected filter options
+  function filterJournals() {
+    const selectedCategory = categorySelect.value;
+    const selectedFilter = topReadsRadio.checked ? "top-reads" : "latest";
 
-  currentIndex = index;
-  const newCenterItem = carouselItems[currentIndex];
-  newCenterItem.classList.add("center-item");
-}
+    // Clear the existing journals in the adjacent element
+    JournalsList.innerHTML = "";
 
-function rotateCarousel(direction) {
-  if (direction === "prev") {
-    currentIndex =
-      currentIndex === 0 ? carouselItems.length - 1 : currentIndex - 1;
-    details[currentIndex].style.display = "flex";
-  } else {
-    currentIndex =
-      currentIndex === carouselItems.length - 1 ? 0 : currentIndex + 1;
-    console.log(currentIndex);
-    details[currentIndex].style.display = "flex";
+    // Filter the journals based on the selected category and filter option
+    const filteredJournals = journals.filter(function (journal) {
+      return (
+        (journal.category === selectedCategory || selectedCategory === "") &&
+        (journal.type === selectedFilter ||
+          (selectedFilter === "top-reads" && journal.type !== "latest"))
+      );
+    });
+
+    // Loop through each filtered journal and insert the corresponding HTML code
+    filteredJournals.forEach(function (journal) {
+      // Construct the HTML code using the object values
+      var htmlCode = `
+        <div class="journal-card">
+          <div class="journal-image">
+            <img src="${journal.previewImage}" alt="Journal" class="Journal-Preview-Image" />
+          </div>
+          <div class="journal-info">
+            <p class="journal-title">${journal.title}</p>
+            <p class="author">Author: <span>${journal.author}</span></p>
+            <p class="publishingdate">Published on: <span>${journal.publishingDate}</span></p>
+            <a href="${journal.url}" class="View-Journal">View</a>
+          </div>
+        </div>
+      `;
+
+      // Insert the HTML code into the adjacent element
+      JournalsList.insertAdjacentHTML("beforeend", htmlCode);
+    });
   }
 
-  showSlide(currentIndex);
+  // Add event listeners to the category select and radio buttons
+  categorySelect.addEventListener("change", filterJournals);
+  topReadsRadio.addEventListener("change", filterJournals);
+  latestRadio.addEventListener("change", filterJournals);
+
+  // Initial call to filter journals based on the default selected options
+  filterJournals();
 }
 
-prevButton.addEventListener("click", () => rotateCarousel("prev"));
-nextButton.addEventListener("click", () => rotateCarousel("next"));
+// Call the function to initialize filtering
+initializeFiltering();
 
-showSlide(currentIndex);
